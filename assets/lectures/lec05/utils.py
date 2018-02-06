e54aa307161500a13bca442592032f8ad1c2d639
Carlo Cruz-Albrecht <Carlo@Carlos-MacBook-Pro.local>
2018-02-05 16:37:23 -0800

    Modified fetch_and_cache so that it displays a progress bar while downloading a file. This version does not use ipywidget but an updating line of text

--- assets/lectures/lec05/utils.py
+++ assets/lectures/lec05/utils.py
@@ -27,6 +27,7 @@ def fetch_and_cache(data_url, file, data_dir="data", force=False):
     """
     import requests
     from pathlib import Path
+    from IPython.display import clear_output
     data_dir = Path(data_dir)
     data_dir.mkdir(exist_ok=True)
     file_path = data_dir/Path(file)
@@ -35,15 +36,21 @@ def fetch_and_cache(data_url, file, data_dir="data", force=False):
     if force and file_path.exists():
         file_path.unlink()
     if force or not file_path.exists():
-        print('Downloading...', end=' ')
-        resp = requests.get(data_url)
+        resp = requests.get(data_url, stream=True)
+        file_size = int(resp.headers.get('content-length', 0))
+        step = 40
+        chunk_size = file_size//step
         with file_path.open('wb') as f:
-            f.write(resp.content)
-        print('Done!')
+            for chunk in resp.iter_content(chunk_size): # write file in chunks
+                f.write(chunk)
+                clear_output(wait=True)
+                step -= 1
+                print('[' + '#'*(41 - step) + (step)*' ' + ']')
+        print("Downloaded " + data_url.split("/")[-1] + "!")
     else:
-        import time 
-        birth_time = time.ctime(file_path.stat().st_ctime)
-        print("Using cached version downloaded:", birth_time)
+        import time
+        time_downloaded = time.ctime(file_path.stat().st_ctime)
+        print("Using version already downloaded:", time_downloaded)
     return file_path
 
 
